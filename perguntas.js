4b77bd6f7714de32a4ba8127a340dbd01080df6f
rute-silva <f.rute@escola.pr.gov.br>
2025-08-08 12:21:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Acidentes de Trânsito',  
+    'O que podemos fazer para diminuir os acidentes de trâsito em nossa cidade?', 
+    ' Respeitas as leis de trânsito, atravessae na faixa, usar cinto de segurança e não usar celular enquanto dirige.' 
+)
+
+criaCartao(
+    'Bombeiros',  
+    'Por que o trabalho dos bombeiros é importante?', 
+    'Porque ajuda em situações de emergência, como acidente, incêndios e resgates.' 
+)
+
+criaCartao(
+    'Matemática no dia a dia (receitas)',
+    'Por que precisamos usar a matemática enquanto queremos dobrar ou reduzir uma receita?',  
+    'Porque precisamos calcular as quantidades certas dos ingredientes para que a rceita fique boa usamos multiplicação e divisão para isso.'
+)
+
+criaCartao(
+    'Dinheiro e descontos', 
+    ' Por que é melhor pagar uma conta no dia certo ou antes do vencimento?', 
+    'Porque assim evitamos pagar juros ou multas, e as vezes ganhamos descontos ' 
+)
